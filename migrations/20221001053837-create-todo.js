d3e682d5cdcc6c5b58ba472454bec3b1c0d2d440
Naveen6626 <112967151+Naveen6626@users.noreply.github.com>
2023-01-07 23:16:51 +0530

    Update 20221001053837-create-todo.js

--- migrations/20221001053837-create-todo.js
+++ migrations/20221001053837-create-todo.js
@@ -2,43 +2,31 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(
-      "Todos",
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        title: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          validate: {
-            notNull: true,
-            len: 5,
-          },
-        },
-        dueDate: {
-          type: Sequelize.DATEONLY,
-        },
-        completed: {
-          type: Sequelize.BOOLEAN,
-          defaultValue: false,
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
+    await queryInterface.createTable("Todos", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
       },
-      {
-        tableName: "todo",
-      }
-    );
+      title: {
+        type: Sequelize.STRING,
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY,
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable("Todos");
